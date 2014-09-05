64f66e07f571e00fa8fb33c1ed03e87454494d5b
apple <apple@apples-Mac-mini.local>
2014-09-05 11:22:39 +0800

    Initial Commit

---
+++ lianlianwin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  lianlianwin
+//
+//  Created by apple on 5/9/14.
+//  Copyright (c) 2014 ___GWH___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ lianlianwin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  lianlianwin
+//
+//  Created by apple on 5/9/14.
+//  Copyright (c) 2014 ___GWH___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
